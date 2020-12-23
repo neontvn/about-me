fbd8e58c422000251cda1dbc746e43f2da27369f
Tanmay Nale <tvnale@gmail.com>
2020-12-23 23:21:48 +0530

    intial setup

--- src/App.js
+++ src/App.js
@@ -7,15 +7,23 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A portfolio website
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/neontvn"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
+        </a>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/tanmay-nale/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin
         </a>
       </header>
     </div>
